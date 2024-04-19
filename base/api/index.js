4b679330fbcc2dcf68362306a68fb317c34daad6
龙孑 <652968195@qq.com>
2024-04-19 22:58:42 +0800

    排版

--- base/api/index.js
+++ base/api/index.js
@@ -4,12 +4,9 @@ export const baseUrl = "http://zyxcl.xyz"
 export const getLoginCellphone = () => {
 	console.log('调用接口');
 	uni.request({
-		// #ifdef H5
-		url: '/music_api/login/cellphone',
-		// #endif
-		// #ifndef H5
+
 		url: baseUrl + '/music_api/login/cellphone',
-		// #endif
+
 		success: res => {
 			console.log(res);
 		},
--- pages/blogs/blogs.vue
+++ pages/blogs/blogs.vue
@@ -1,18 +1,31 @@
 <template>
 	<view>
-		博客
+		<view class="header">
+		</view>
+		<view class="main">
+			<recommend></recommend>
+			<!-- <audioBook></audioBook> -->
+		</view>
+
 	</view>
 </template>
 
 <script>
+	import recommend from '@/pages/blogs/components/recommend/recommend.vue'
+	import audioBook from '@/pages/blogs/components/audioBook/audioBook.vue'
+
 	export default {
+		components: {
+			audioBook,
+			recommend
+		},
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
