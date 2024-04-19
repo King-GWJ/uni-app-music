d0c8eb1bd3eb9e437b95780156e20d3eb2f4f6ff
gaowanjia <884923222@qq.com>
2024-04-19 17:53:39 +0800

    封装网络请求方法

--- base/api/index.js
+++ base/api/index.js
@@ -1,20 +1,28 @@
-export const baseUrl = "http://zyxcl.xyz"
+export const baseUrl = "http://zyxcl.xyz/music_api"
 
-
-export const getLoginCellphone = () => {
-	console.log('调用接口');
-	uni.request({
-		// #ifdef H5
-		url: '/music_api/login/cellphone',
-		// #endif
-		// #ifndef H5
-		url: baseUrl + '/music_api/login/cellphone',
-		// #endif
-		success: res => {
-			console.log(res);
-		},
-		fail: err => {
-			console.log(err);
-		}
+// 封装请求函数
+export const request = ({ url, method = 'GET', data = {}, header = {} }) => {
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: baseUrl + url,
+			method,
+			data: {
+				cookie: uni.getStorageSync('curCookie') || '',
+				...data
+			},
+			header,
+			withCredentials: true, // 跨域请求携带 cookie
+			success: res => {
+				resolve(res.data)
+			},
+			fail: err => {
+				reject(err)
+			}
+		})
 	})
+}
+
+//轮播图
+export const bannerApi = () => {
+	return request({ url: '/banner' })
 }
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,47 +1,19 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view>
+    <view class="header">
+      <uni-search-bar placeholder="搜索" bgColor="#EEEEEE" />
+    </view>
+  </view>
 </template>
+<script setup>
+  import {ref} from "vue";
 
-<script>
-
-	export default {
-		data() {
-			return {
-				title: 'Hello123'
-			}
-		},
-		methods: {
-
-		}
-	}
+  ref([])
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-    margin: 200rpx auto 50rpx;
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    align-items: center;
   }
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
