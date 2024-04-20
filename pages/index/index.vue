a3d41403f5572348302eefa3e73df1cacfe6eaec
龙孑 <652968195@qq.com>
2024-04-20 08:31:04 +0800

    .

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,47 +1,82 @@
+<script setup>
+	import {
+		ref
+	} from "vue";
+	import {
+		bannerApi
+	} from '/base/api'
+
+	const banners = ref([])
+
+	bannerApi().then(res => {
+		banners.value = res.banners
+	})
+</script>
+
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="header">
+			<uni-icons class="bars" type="bars" size="24" @click="userDrawer.open()"></uni-icons>
+			<view class="search" @click="uni.navigateTo({url: '/pages/search/search'})">
+				<uni-search-bar placeholder="搜索" bgColor="#EEEEEE" readonly />
+			</view>
+		</view>
+		<view class="main">
+			<view class="swiper-wrap">
+				<swiper class="swiper" indicator-dots indicator-color="rgba(255, 255, 255, .4)"
+					indicator-active-color="#ffffff" autoplay circular>
+					<swiper-item v-for="item in banners" :key="item.targetId">
+						<image :src="item.imageUrl" mode="widthFix"></image>
+					</swiper-item>
+				</swiper>
+			</view>
 		</view>
 	</view>
 </template>
 
-<script>
+<style lang="scss" scoped>
+	.content {
+		width: 100%;
+		height: 100%;
 
-	export default {
-		data() {
-			return {
-				title: 'Hello123'
+		.header {
+			padding: 30rpx;
+			display: flex;
+			width: 100%;
+			box-sizing: border-box;
+			align-items: center;
+
+			.search {
+				flex: 1;
+				border-radius: 40rpx;
+				margin-left: 20rpx;
+
+				.uni-searchbar {
+					padding: 0;
+				}
 			}
-		},
-		methods: {
+		}
+
+		.main {
+			width: 100%;
+			height: 100%;
+
+			.swiper-wrap {
+				padding: 30rpx;
+
+				.swiper {
+					border-radius: 20rpx;
+					overflow: hidden;
+					height: 256rpx;
+
+					image {
+						width: 100%;
+					}
+				}
+			}
+
 
 		}
-	}
-</script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-    margin: 200rpx auto 50rpx;
-  }
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
 	}
 </style>
