6790120cf5dfaebb8694e0d8a6f3509301abc247
gaowanjia <884923222@qq.com>
2024-04-20 10:01:16 +0800

    同意 缩进符号

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import {createSSRApp} from 'vue'
 import * as Pinia from 'pinia';
 
 export function createApp() {
-  const app = createSSRApp(App)
-  app.use(Pinia.createPinia())
-  return {
-    app,
-    Pinia
-  }
+    const app = createSSRApp(App)
+    app.use(Pinia.createPinia())
+    return {
+        app,
+        Pinia
+    }
 }
--- store/user.js
+++ store/user.js
@@ -2,42 +2,42 @@ import {defineStore} from 'pinia'
 import {ref} from 'vue'
 import {loginApi, loginStatusApi} from '/base/api'
 
-
 export const useUserStore = defineStore('user', () => {
-  const formData = ref({
-    email: '',
-    password: ''
-  })
-  //用户信息
-  const userInfo = ref(null)
-
-  const profile = ref(null)
-
-  const getProfile = () => {
-    loginStatusApi().then(res => {
-      console.log("ggg:::",res)
-      profile.value = res.data.profile
-      profile.value = res.data.account
+    const formData = ref({
+        email: '',
+        password: ''
     })
-  }
 
-  const getLogin = (email, password) => {
-    loginApi(email, password).then(res=>{
-      console.log("ggg:",res)
-      uni.setStorageSync('userCookie', res.cookie)
-      uni.setStorageSync('userToken', res.token)
-      userInfo.value = res.account
-      profile.value = res.profile
-      getProfile()
-    })
-  }
+    //用户信息
+    const userInfo = ref(null)
+
+    const profile = ref(null)
+
+    const getProfile = () => {
+        loginStatusApi().then(res => {
+            console.log("ggg:::", res)
+            profile.value = res.data.profile
+            profile.value = res.data.account
+        })
+    }
+
+    const getLogin = (email, password) => {
+        loginApi(email, password).then(res => {
+            console.log("ggg:", res)
+            uni.setStorageSync('userCookie', res.cookie)
+            uni.setStorageSync('userToken', res.token)
+            userInfo.value = res.account
+            profile.value = res.profile
+            getProfile()
+        })
+    }
 
 
-  return {
-    formData,
-    userInfo,
-    profile,
-    getProfile,
-    getLogin
-  }
+    return {
+        formData,
+        userInfo,
+        profile,
+        getProfile,
+        getLogin
+    }
 });
