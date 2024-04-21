e7641b3781b9605394186abba39507fa61cb4106
gaowanjia <884923222@qq.com>
2024-04-21 22:51:49 +0800

    添加退出登录按钮

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,93 +1,4 @@
 <script setup>
-<<<<<<< HEAD
-	import {
-		ref
-	} from "vue";
-	import {
-		bannerApi
-	} from '/base/api'
-
-	const banners = ref([])
-
-	const pageLogin = '/pages/login/login'
-	const pageSearch = '/pages/search/search'
-
-	bannerApi().then(res => {
-		banners.value = res.banners
-	})
-
-	const link = (url) => {
-		if (!url) {
-			return
-		}
-		uni.navigateTo({
-			url
-		})
-	}
-</script>
-
-<template>
-	<view class="content">
-		<view class="header">
-			<uni-icons class="bars" type="bars" size="24" @click="link(pageLogin)"></uni-icons>
-			<view class="search" @click="link(pageSearch)">
-				<uni-search-bar placeholder="搜索" bgColor="#EEEEEE" readonly />
-			</view>
-		</view>
-		<view class="main">
-			<view class="swiper-wrap">
-				<swiper class="swiper" indicator-dots indicator-color="rgba(255, 255, 255, .4)"
-					indicator-active-color="#ffffff" autoplay circular>
-					<swiper-item v-for="item in banners" :key="item.targetId">
-						<image :src="item.imageUrl" mode="widthFix"></image>
-					</swiper-item>
-				</swiper>
-			</view>
-		</view>
-	</view>
-</template>
-
-<style lang="scss" scoped>
-	.content {
-		width: 100%;
-		height: 100%;
-
-		.header {
-			padding: 30rpx;
-			display: flex;
-			width: 100%;
-			box-sizing: border-box;
-			align-items: center;
-
-			.search {
-				flex: 1;
-				border-radius: 40rpx;
-				margin-left: 20rpx;
-
-				.uni-searchbar {
-					padding: 0;
-				}
-			}
-		}
-
-		.main {
-			width: 100%;
-			height: 100%;
-
-			.swiper-wrap {
-				padding: 30rpx;
-
-				.swiper {
-					border-radius: 20rpx;
-					overflow: hidden;
-					height: 256rpx;
-
-					image {
-						width: 100%;
-					}
-				}
-			}
-=======
     import {ref} from "vue";
     import {bannerApi} from '/base/api'
     import {link} from '/base/utils'
@@ -156,6 +67,10 @@
                 }">
                     <text class="word-btn-white">登录</text>
                 </button>
+                <button @click="()=>{
+                }">
+                    <text class="word-btn-white">退出登录</text>
+                </button>
             </view>
         </uni-drawer>
     </view>
@@ -173,7 +88,6 @@
             width: 100%;
             box-sizing: border-box;
             align-items: center;
->>>>>>> foreshow
 
             .search {
                 flex: 1;
@@ -189,8 +103,6 @@
         .main {
             width: 100%;
             height: 100%;
-
-
 
             .swiper-wrap {
                 padding: 30rpx;
@@ -232,14 +144,7 @@
 
         }
 
-<<<<<<< HEAD
-		}
-
-	}
-</style>
-=======
     }
-// 测试
+
 
 </style>
->>>>>>> foreshow
