fc99c2583792b5075af3cd9a2fda5ae7270410f9
gaowanjia <884923222@qq.com>
2024-04-21 23:54:27 +0800

    app.vue冲突

--- App.vue
+++ App.vue
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-
-	}
-</script>
-
-<style lang="scss">
-	// @import "/uni_modules/uni-scss/index.scss";
-	/*每个页面公共css */
-	page,
-	uni-page-body {
-		height: 100%;
-	}
-</style>
-=======
 <script setup>
     import {onLaunch} from '@dcloudio/uni-app'
     import {useUserStore} from './store/user'
