6360c89aa5bdf9ea478074d8716637f5a8e4c5e3
lxt <1425351337@qq.com>
2024-04-24 23:14:43 +0800

    1

--- components/musicBar/musicBar.vue
+++ components/musicBar/musicBar.vue
@@ -1,54 +1,68 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { useMusicstore } from '../../store/music';
-import {
-    onShow
-  } from "@dcloudio/uni-app";
+	import {
+		ref,
+		watch
+	} from 'vue';
+	import {
+		useMusicstore
+	} from '../../store/music';
+	import {
+		onLaunch,
+		onShow
+	} from "@dcloudio/uni-app";
 
-const musicStore = useMusicstore()
-const url = ref('')
-const name = ref('请播放音乐')
-const artist = ref('')
-const flag = ref(true)
+	const musicStore = useMusicstore()
+	const url = ref('')
+	const name = ref('请播放音乐')
+	const artist = ref('')
+	const flag = ref(true)
 
-const goPlay = () =>{
-	uni.switchTab({
-		url: `/pages/musicPlay/musicPlay`
-	})
-}
+	const goPlay = () => {
+		uni.switchTab({
+			url: `/pages/musicPlay/musicPlay`
+		})
+	}
 
-musicStore.audio.onPlay(()=>{
+	musicStore.audio.onPlay(() => {
+
 		url.value = musicStore.musicLove.al.picUrl
 		name.value = musicStore.musicLove.name
 		artist.value = musicStore.musicLove.ar[0].name
 		flag.value = false
-})
+	})
 
-musicStore.audio.onPause(()=>{
+	musicStore.audio.onPause(() => {
 		flag.value = true
 		url.value = musicStore.musicLove.al.picUrl
 		name.value = musicStore.musicLove.name
 		artist.value = musicStore.musicLove.ar[0].name
 
-})
+	})
 
-onShow(()=>{
-	url.value = musicStore.musicLove.al.picUrl
-	name.value = musicStore.musicLove.name
-	artist.value = musicStore.musicLove.ar[0].name
-})
 
-const change = (e)=>{
-	musicStore.play()
-	flag.value = !flag.value
-	
-	e.stopPropagation()
-}
+	onShow(() => {
 
-const detail = (e)=>{
-	e.stopPropagation()
-}
+		url.value = musicStore.musicLove.al.picUrl
+		name.value = musicStore.musicLove.name
+		artist.value = musicStore.musicLove.ar[0].name
 
+
+		if (musicStore.audio.paused) {
+			flag.value = true
+		} else {
+			flag.value = false
+		}
+	})
+
+	const change = (e) => {
+		musicStore.play()
+		flag.value = !flag.value
+		e.stopPropagation()
+	}
+
+	const detail = (e) => {
+		e.stopPropagation()
+	}
 </script>
 
 <template>
@@ -57,21 +71,21 @@ const detail = (e)=>{
 			<view class="imgWrap">
 				<image class="img" :src="url" mode="widthFix"></image>
 			</view>
-			
+
 			<view class="name">
-				{{name}} <view v-if="musicStore.musicLove.al">
+				{{name}}
+				<view v-if="musicStore.musicLove.al">
 					-{{artist}}
 				</view>
 			</view>
 		</view>
-		
+
 		<view class="barTool">
-			<!-- <view :class={play:!musicStore.musicLove.al,pause:musicStore.musicLove.al} @click="change"> -->
 			<view :class="[{ play: flag }, { pause: !flag }]" @click="change">
-				
+
 			</view>
-			<view class="list"  @click="detail">
-				
+			<view class="list" @click="detail">
+
 			</view>
 		</view>
 	</view>
@@ -80,70 +94,72 @@ const detail = (e)=>{
 
 
 <style lang="scss" scoped>
-
-.musicBar{
-	position: absolute;
-	bottom: rpx(0);
-	width: 100%;
-	height: rpx(50);
-	z-index: 9;
-	background: white;
-	display: flex;
-	justify-content: space-between;
-	.barInfo{
+	.musicBar {
+		position: absolute;
+		bottom: rpx(0);
+		width: 100%;
+		height: rpx(50);
+		z-index: 9;
+		background: white;
 		display: flex;
-		align-items: center;
-		padding-left: rpx(10);
-		.imgWrap{
-			width: rpx(35);
-			height: rpx(35);
-			border: 3px solid;
-			border-radius: rpx(50);
-			background: #ccc;
-		}
-		.img{
-			width: rpx(35);
-			height: rpx(35);
-			border-radius: rpx(50);
-		}
-		.name{
-			line-height: rpx(50);
-			margin-left: rpx(10);
-			color: rgb(61,70,91);
-			font-size: rpx(10);
-			white-space: nowrap;
-			overflow: hidden;
-			text-overflow: ellipsis;
+		justify-content: space-between;
+
+		.barInfo {
 			display: flex;
+			align-items: center;
+			padding-left: rpx(10);
+
+			.imgWrap {
+				width: rpx(35);
+				height: rpx(35);
+				border: 3px solid;
+				border-radius: rpx(50);
+				background: #ccc;
+			}
+
+			.img {
+				width: rpx(35);
+				height: rpx(35);
+				border-radius: rpx(50);
+			}
+
+			.name {
+				line-height: rpx(50);
+				margin-left: rpx(10);
+				color: rgb(61, 70, 91);
+				font-size: rpx(10);
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				display: flex;
+			}
 		}
+
+		.barTool {
+			display: flex;
+			margin-right: rpx(10);
+
+			.play {
+				width: rpx(50);
+				height: rpx(50);
+				background: url(../../icon/play.svg) no-repeat center;
+				background-size: rpx(25);
+			}
+
+			.pause {
+				width: rpx(50);
+				height: rpx(50);
+				background: url(../../icon/songlist/icon-a1.png) no-repeat center;
+				background-size: rpx(25);
+			}
+
+			.list {
+				width: rpx(50);
+				height: rpx(50);
+				background: url(../../icon/songlist/icon-shlb.png) no-repeat center;
+				background-size: rpx(20);
+			}
+		}
+
 	}
-	.barTool{
-		display: flex;
-		margin-right: rpx(10);
-		.play{
-			width: rpx(50);
-			height: rpx(50);
-			background: url(../../icon/play.svg) no-repeat center;
-			background-size: rpx(25);
-		}
-		.pause{
-			width: rpx(50);
-			height: rpx(50);
-			background: url(../../icon/songlist/icon-a1.png) no-repeat center;
-			background-size: rpx(25);
-		}
-		.list{
-			width: rpx(50);
-			height: rpx(50);
-			background: url(../../icon/songlist/icon-shlb.png) no-repeat center;
-			background-size: rpx(20);
-		}
-	}
-	
-}
-
-
-
-
-
 </style>
