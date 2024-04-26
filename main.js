9adb823613aaef3a6b02c28e5692c4a91b1cb575
gaowanjia <884923222@qq.com>
2024-04-26 09:07:26 +0800

    添加 vconsole插件

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import App from './App'
 import {createSSRApp} from 'vue'
 import * as Pinia from 'pinia';
+import * as Vconsole from "vconsole";
 
 export function createApp() {
     const app = createSSRApp(App)
+    const vConsole = new Vconsole()
     app.use(Pinia.createPinia())
     return {
         app,
-        Pinia
+        Pinia,
+        vConsole
     }
 }
